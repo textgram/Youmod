baefdd1ed99ad9e8c98eaea8774315b5b1f0efec
textgram <alishaeow@gmail.com>
2026-08-27 09:40:06 +0530

    Create BootReceiver.kt

---
+++ app/src/main/java/com/example/youmod/BootReceiver.kt
@@ -0,0 +1,18 @@
+package com.example.youmod
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == "android.intent.action.BOOT_COMPLETED") {
+            val serviceIntent = Intent(context, BotService::class.java)
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                context.startForegroundService(serviceIntent)
+            } else {
+                context.startService(serviceIntent)
+            }
+        }
+    }
+}
